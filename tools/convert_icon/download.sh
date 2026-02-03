16dab121c71176ba477bc7b00315be4ca3664718
Stefan Zellmann <info@szellmann.de>
2026-02-03 21:32:35 +0100

    Tweak icon converter scripts

--- tools/convert_icon/download.sh
+++ tools/convert_icon/download.sh
@@ -1,32 +1,34 @@
-vars='U V W'
-#for var in $vars;
-#do
-#  #for i in $(seq 1 120);
-#  for i in $(seq 1 5);
-#  do
-#    wget https://opendata.dwd.de/weather/nwp/icon/grib/00/${var,,}/icon_global_icosahedral_model-level_2026020200_000_${i}_${var}.grib2.bz2
-#    bzip2 -d icon_global_icosahedral_model-level_2026020200_000_${i}_${var}.grib2.bz2
-#    rm -rf icon_global_icosahedral_model-level_2026020200_000_${i}_${var}.grib2.bz2
-#    cdo -f nc copy icon_global_icosahedral_model-level_2026020200_000_${i}_${var}.grib2 icon_global_icosahedral_model-level_2026020200_000_${i}_${var}.nc
-#    rm -rf icon_global_icosahedral_model-level_2026020200_000_${i}_${var}.grib2
-#  done
-#done
+date='2026020300'
 
-#for i in $(seq 1 120);
-for i in $(seq 1 5);
+vars='U V W'
+for var in $vars;
 do
-  wget https://opendata.dwd.de/weather/nwp/icon/grib/00/hhl/icon_global_icosahedral_time-invariant_2026020200_${i}_HHL.grib2.bz2
-  bzip2 -d icon_global_icosahedral_time-invariant_2026020200_${i}_HHL.grib2.bz2
-  rm -rf icon_global_icosahedral_time-invariant_2026020200_${i}_HHL.grib2.bz2
-  cdo -f nc copy icon_global_icosahedral_time-invariant_2026020200_${i}_HHL.grib2 icon_global_icosahedral_time-invariant_2026020200_${i}_HHL.nc
-  rm -rf icon_global_icosahedral_time-invariant_2026020200_${i}_HHL.grib2
+  #for i in $(seq 1 120);
+  for i in $(seq 1 8);
+  do
+    wget https://opendata.dwd.de/weather/nwp/icon/grib/00/${var,,}/icon_global_icosahedral_model-level_${date}_000_${i}_${var}.grib2.bz2
+    bzip2 -d icon_global_icosahedral_model-level_${date}_000_${i}_${var}.grib2.bz2
+    rm -rf icon_global_icosahedral_model-level_${date}_000_${i}_${var}.grib2.bz2
+    cdo -f nc copy icon_global_icosahedral_model-level_${date}_000_${i}_${var}.grib2 icon_global_icosahedral_model-level_${date}_000_${i}_${var}.nc
+    rm -rf icon_global_icosahedral_model-level_${date}_000_${i}_${var}.grib2
+  done
 done
 
-wget https://opendata.dwd.de/weather/nwp/icon/grib/00/hsurf/icon_global_icosahedral_time-invariant_2026020200_HSURF.grib2.bz2
-bzip2 -d icon_global_icosahedral_time-invariant_2026020200_HSURF.grib2.bz2
-rm -rf icon_global_icosahedral_time-invariant_2026020200_HSURF.grib2.bz2
-cdo -f nc copy icon_global_icosahedral_time-invariant_2026020200_HSURF.grib2 icon_global_icosahedral_time-invariant_2026020200_HSURF.nc
-rm -rf icon_global_icosahedral_time-invariant_2026020200_HSURF.grib2
+#for i in $(seq 1 120);
+for i in $(seq 1 8);
+do
+  wget https://opendata.dwd.de/weather/nwp/icon/grib/00/hhl/icon_global_icosahedral_time-invariant_${date}_${i}_HHL.grib2.bz2
+  bzip2 -d icon_global_icosahedral_time-invariant_${date}_${i}_HHL.grib2.bz2
+  rm -rf icon_global_icosahedral_time-invariant_${date}_${i}_HHL.grib2.bz2
+  cdo -f nc copy icon_global_icosahedral_time-invariant_${date}_${i}_HHL.grib2 icon_global_icosahedral_time-invariant_${date}_${i}_HHL.nc
+  rm -rf icon_global_icosahedral_time-invariant_${date}_${i}_HHL.grib2
+done
+
+wget https://opendata.dwd.de/weather/nwp/icon/grib/00/hsurf/icon_global_icosahedral_time-invariant_${date}_HSURF.grib2.bz2
+bzip2 -d icon_global_icosahedral_time-invariant_${date}_HSURF.grib2.bz2
+rm -rf icon_global_icosahedral_time-invariant_${date}_HSURF.grib2.bz2
+cdo -f nc copy icon_global_icosahedral_time-invariant_${date}_HSURF.grib2 icon_global_icosahedral_time-invariant_${date}_HSURF.nc
+rm -rf icon_global_icosahedral_time-invariant_${date}_HSURF.grib2
 
 # horizontal grid:
 wget http://icon-downloads.mpimet.mpg.de/grids/public/edzw/icon_grid_0026_R03B07_G.nc
--- tools/convert_icon/exec.sh
+++ tools/convert_icon/exec.sh
@@ -1,1 +1,2 @@
-./tools/convert_icon/convert_icon -hgrid ./icon_grid_0026_R03B07_G.nc -hsurf ./icon_global_icosahedral_time-invariant_2026020200_HSURF.nc -hhl ./icon_global_icosahedral_time-invariant_2026020200*_HHL.nc -u ./icon_global_icosahedral_model-level_2026020200_000*_U.nc -v ./icon_global_icosahedral_model-level_2026020200_000*_V.nc -w ./icon_global_icosahedral_model-level_2026020200_000*_W.nc
+date='2026020300'
+./tools/convert_icon/convert_icon -hgrid ./icon_grid_0026_R03B07_G.nc -hsurf ./icon_global_icosahedral_time-invariant_${date}_HSURF.nc -hhl ./icon_global_icosahedral_time-invariant_${date}*_HHL.nc -u ./icon_global_icosahedral_model-level_${date}_000*_U.nc -v ./icon_global_icosahedral_model-level_${date}_000*_V.nc -w ./icon_global_icosahedral_model-level_${date}_000*_W.nc
